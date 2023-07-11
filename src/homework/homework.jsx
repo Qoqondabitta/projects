ffb048209e4052a3d7c58769a00917c99329dc10
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-11 17:29:37 +0500

    components and props

--- src/homework/homework.jsx
+++ src/homework/homework.jsx
@@ -3,6 +3,16 @@ import React from "react"
 
 export class Navbar extends React.Component{
     render(){
-        return<h1>Navbar</h1>
+        console.log(this);
+        // const {
+        //     data:{name, title}
+        // } = this.props
+        return<div>
+            <div className="fruits">                
+            <h3>{this.props.data[0]}</h3>
+            <h3>{this.props.data[1]}</h3>
+            <h3>{this.props.data[2]}</h3>
+            </div>
+        </div>
     }
 }
