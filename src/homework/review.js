ea4ba444b7d0c870730d126a0cabafda55c5fd74
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-11 19:54:58 +0500

    components and props homework

--- src/homework/review.js
+++ src/homework/review.js
@@ -136,3 +136,28 @@
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+// =======================================================================================
+// ================================July 11, 2023==========================================
+// =======================================================================================
+// import React from "react"
+
+
+// export class Navbar extends React.Component{
+//     render(){
+//         console.log(this);
+//         const {
+//             data:{name, title}
+//         } = this.props
+//         return<div>
+//             {/* <div className="fruits">                
+//             <h3>{this.props.data[0]}</h3>
+//             <h3>{this.props.data[1]}</h3>
+//             <h3>{this.props.data[2]}</h3>
+//             </div> */}
+//             {/* {this.props.data.map((item)=>{
+//                 return item
+//             })} */}
+//             {/* <h1>{this.props.children}</h1> */}
+//         </div>
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Navbar } from './homework/homework';
+import { Navbar } from './homework/Component/homework';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Navbar data={['Home', 'Service', 'About']}>
-        salom
-      </Navbar>
-      {/* <Navbar info="firstProp" /> */}
+      <Navbar />
   </React.StrictMode>
 );
 
