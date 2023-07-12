e0bc2a6971e0767036964c145161017d1ae35b78
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-12 22:19:46 +0500

    state 1 and 2 advanced

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import StateLesson from './state/state';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Navbar />
       <StateLesson />
+      <Navbar />
   </React.StrictMode>
 );
 
--- src/state/state.jsx
+++ src/state/state.jsx
@@ -16,7 +16,9 @@ export class StateLesson extends React.Component{
             typedValue: '',
             surname:'',
             firstName:'',
-            students: database
+            students: database,
+            isDark: false,
+            language: 'uz'
         }
     }
     render(){
@@ -36,8 +38,29 @@ export class StateLesson extends React.Component{
             let filtered= database.filter(student=> student.name.toLowerCase().includes(value.toLowerCase()) )
             this.setState({students: filtered})
         }
+        const onCheckbox=()=>{
+            this.setState({
+                isDark: !this.state.isDark
+            })
+        }
+        const selectLanguage=({target:{value}})=>{
+            this.setState({language: value})
+        }
+        const sayHello=(tongue)=>{
+                return tongue==='uz'?"Assalomu alekum":tongue==='ru'?'Privet':tongue==='en'?'Hello':'Select'
+        }
         return(
-            <div>
+            <div style={
+                {
+                    position: "absolute",
+                    top: 0,
+                    // bottom: 0,
+                    right: 0,
+                    left: 0,
+                    background: this.state.isDark?"black":'white',
+                    color: this.state.isDark?"white":"black"
+                }
+            }>
                 <h1>{this.state.count}</h1>
                 <h1>{this.state.typedValue}</h1>
                 <button onClick={plus}>Plus</button>
@@ -49,6 +72,14 @@ export class StateLesson extends React.Component{
                 <input type="text" placeholder="surname" onChange={onChangeInfo} />
                 <input type="text" placeholder="find" onChange={onSearch} />
                 {this.state.students.map(v => <h1>{v.id}. {v.surname} {v.name}</h1>)}
+                <input type="checkbox" onClick={onCheckbox} />{""}
+                <span>{this.state.isDark?'Tungi Rejim':'Kungi Rejim'}</span>
+                <select name="" id="" onChange={selectLanguage}>
+                    <option value="uz">Uzbek</option>
+                    <option value="ru">Russian</option>
+                    <option value="en">English</option>
+                </select>
+                <h2>{sayHello(this.state.language)}</h2>
             </div>
             )
     }
