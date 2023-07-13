def282b5129e7387124305d42e78b28d8356d010
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-14 00:42:44 +0500

    styled components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 // import { Navbar } from './homework/Component/homework';
 // import StateLesson from './state/state';
 // import {HomeworkComponents} from './state/state'
-import {HomeworkState} from './state/state'
+// import {HomeworkState} from './state/state'
+import Components5 from './styledComponents'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HomeworkState />
+    <Components5 />
+      {/* <HomeworkState /> */}
       {/* <HomeworkComponents data={['home', 'about', 'contact', 'service']}> and this is children </HomeworkComponents> */}
       {/* <StateLesson /> */}
       {/* <Navbar /> */}
---
+++ src/styledComponents/styledComponents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Title} from './styledComponents'
+
+export class StyledComponents5 extends React.Component{
+    render(){
+        return <div>
+            <h1>Styled Components 5</h1>
+            <Title>Styled Components 5</Title>
+        </div>;
+    }
+}
+export default StyledComponents5
