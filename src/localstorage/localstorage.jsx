52b52a8e13087807430b597e31d8e31bdb8f51ad
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-24 16:45:07 +0500

    localStorage advanced

--- src/localstorage/localstorage.jsx
+++ src/localstorage/localstorage.jsx
@@ -1,17 +1,29 @@
 import React, {useState} from 'react'
 
 // localStorage.setItem("aim", "perfectness")
+localStorage.setItem("aim", JSON.stringify({}))
 export const Localstorage = () => {
     const [aim, setAim] = useState(localStorage.getItem("aim"))
+    // const [digit, setDigit] = useState(localStorage.getItem("digit"))
+    // const Plus = ()=>{
+    //   localStorage.setItem("digit")
+    // }
+    const createUser = (name)=>{
+      return {id: Date.now().toString().splice(0, 3), name}
+    }
   return (
     <div>
         <h1>Localstorage</h1>
-        <h2>{localStorage.getItem("aim")}</h2>
+        {/* <h2>{localStorage.getItem("digit")}</h2> */}
+        <h2>{JSON.parse(localStorage.getItem("aim")).name}</h2>
         <input type="text" onChange={ ({target:{value}}) => {
             setAim(value);
-            localStorage.setItem('aim', value)
+            let res =createUser(value)
+            localStorage.setItem('aim', JSON.stringify(res))
+            // localStorage.setItem('aim', value)
         } } />
-        <button onClick={()=>{localStorage.removeItem('aim');setAim('')}}>Delete</button>
+        {/* <h2>{digit}</h2> */}
+        {/* <button onClick={()=>{localStorage.removeItem(JSON.parse(localStorage.getItem("aim")));setAim('')}}>Delete</button> */}
     </div>
   )
 }
