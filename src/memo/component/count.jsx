031623b7d49ebe92fc3a05ced5c3e21b537fbc98
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-27 19:06:42 +0500

    react memo full video tutorial

--- src/memo/component/count.jsx
+++ src/memo/component/count.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export const Count = (props) => {
+export const Count = React.memo( (props) =>{
+  console.log(`${props.id}`);
   return <span>{props.count}</span>
-}
+}, (prevProps, nextProps) => {
+  console.log(prevProps);
+  console.log(nextProps);
+} )
 
 export default Count
