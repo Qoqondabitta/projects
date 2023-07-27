fe656fa0a004ae404be62a9040a0276d6d2a81cb
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-07-27 18:58:13 +0500

    react memo full

--- src/memo/homework/homecomponent/count.jsx
+++ src/memo/homework/homecomponent/count.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export const Count = (props) => {
+export const Count = React.memo( (props) =>{
+    console.log(`${props.id} yes`);
   return (
     <div>{props.count}</div>
   )
-}
+}, (prevProps, nextProps) => {
+    return console.log(prevProps, nextProps);
+});
 
 export default Count
